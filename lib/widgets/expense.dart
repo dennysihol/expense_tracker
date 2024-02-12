a4ba23b4d232d0b7a4c64d74805c7d701062b548
dennysihol <denny.sihol@gmail.com>
2024-02-12 14:29:41 +0700

    expense item custom design

---
+++ lib/widgets/expense.dart
@@ -0,0 +1,45 @@
+import 'package:expense_tracker/widgets/expense_list/expenses_list.dart';
+import 'package:expense_tracker/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpense = [
+    Expense(
+      title: 'Flutter course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15,
+      date: DateTime.now(),
+      category: Category.leisure,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children: [
+        const Text(
+          'Chart',
+        ),
+        Expanded(
+          child: ExpensesList(
+            expenses: _registeredExpense,
+          ),
+        ),
+      ]),
+    );
+  }
+}
